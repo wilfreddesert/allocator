199a245e2eeb9c74c048f04f149c4c57a32c4e80
hoooouston <45564306+hoooouston@users.noreply.github.com>
2018-12-04 22:05:09 +0500

    Add files via upload

--- malloc_test.c
+++ malloc_test.c
@@ -6,18 +6,16 @@
 
 int main(void)
 {
-	void * my_block = malloc(MALLOC_SIZE); // For host system demonstration, alloc 4MB block
+	void * my_block = malloc(MALLOC_SIZE); 
 
 	printf("Allocated a block on the host, pointer: %p, size %d\n", my_block, MALLOC_SIZE);
 
-	//Initialize our simple freelist with the space.
-	//On our embedded platform, this will be the RAM address for your pool and the size you desire.
 	malloc_addblock(my_block, MALLOC_SIZE);
 
 	void *t1 = fl_malloc(100);
 	printf("Malloc'd from free list: %p\n", t1);
 
-	void * t2 = fl_malloc(4 * 1024);
+	void * t2 = fl_malloc(100);
 	printf("Malloc'd from free list: %p\n", t2);
 
 	printf("Putting first pointer back into the pool\n");
@@ -30,6 +28,7 @@ int main(void)
 
 	fl_free(t2), t2 = NULL;
 	fl_free(t1), t1 = NULL;
-
+	fl_realloc(my_block, 2*MALLOC_SIZE);
 	free(my_block);
+	getch();
 }
